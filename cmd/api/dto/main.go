cf665d15aaa206e2ced9ed29d5d79850fcbf7e97
ndfsa <25588077+ndfsa@users.noreply.github.com>
2023-08-20 21:05:55 -0400

    add service dto

--- cmd/api/dto/main.go
+++ cmd/api/dto/main.go
@@ -1,7 +1,16 @@
 package dto
 
+import "github.com/shopspring/decimal"
+
 type UserDto struct {
 	Fullname string `json:"name"`
 	Username string `json:"user"`
 	Password string `json:"pass"`
+}
+
+type ServiceDto struct {
+	Type        uint8           `json:"type"`
+	State       uint8           `json:"state"`
+	Currency    string          `json:"currency"`
+	InitBalance decimal.Decimal `json:"init_balance"`
 }
