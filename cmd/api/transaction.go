b4f5cba49d09f933da7a6a3f028cc23f9c6014e8
ndfsa <25588077+ndfsa@users.noreply.github.com>
2023-08-25 22:50:24 -0400

    transaction endpoints

---
+++ cmd/api/transaction.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"database/sql"
+	"net/http"
+
+	"github.com/ndfsa/backend-test/internal/middleware"
+)
+
+func CreateTransactionRoutes(db *sql.DB, baseUrl string) {
+	http.Handle(baseUrl+"/transaction", middleware.Chain(
+		middleware.Logger,
+		middleware.Method(http.MethodGet),
+		middleware.Auth)(getTransaction(db)))
+	http.Handle(baseUrl+"/transaction/execute", middleware.Chain(
+		middleware.Logger,
+		middleware.Method(http.MethodPost),
+		middleware.Auth)(executeTransaction(db)))
+	http.Handle(baseUrl+"/transaction/rollback", middleware.Chain(
+		middleware.Logger,
+		middleware.Method(http.MethodDelete),
+		middleware.Auth)(rollbackTransaction(db)))
+}
+
+func getTransaction(db *sql.DB) http.HandlerFunc {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+	})
+}
+
+func executeTransaction(db *sql.DB) http.HandlerFunc {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+	})
+}
+
+func rollbackTransaction(db *sql.DB) http.HandlerFunc {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+	})
+}
