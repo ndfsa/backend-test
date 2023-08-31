5b44b9a587fbb88c3e33bd2949ca65e544c1e0a1
ndfsa <25588077+ndfsa@users.noreply.github.com>
2023-08-30 21:40:45 -0400

    server struct

--- cmd/load_balancer/main.go
+++ cmd/load_balancer/main.go
@@ -1,11 +1,16 @@
 package main
 
 func main() {
-    // create an http server
+	// create an http server
 
-    // endpoint to register server
-    // research security
+	// endpoint to register server
+	// research security
 
-    // http client to send pings
+	// http client to send pings
 }
 
+type Server struct {
+	Url    string
+	Weight int
+	State  int
+}
