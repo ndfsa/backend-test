22ed562310ddafb21030fde160573417715255d7
ndfsa <25588077+ndfsa@users.noreply.github.com>
2023-08-31 23:13:21 -0400

    changes to load balancer

--- cmd/load_balancer/main.go
+++ cmd/load_balancer/main.go
@@ -1,12 +1,22 @@
 package main
 
+import "net/http"
+
+var servers []Server = make([]Server, 0)
+
 func main() {
-	// create an http server
 
 	// endpoint to register server
+	http.HandleFunc("/register", func(w http.ResponseWriter, r *http.Request) {
+		servers = append(servers, Server{Url: r.RemoteAddr, Weight: 1, State: 1})
+	})
+
 	// research security
 
-	// http client to send pings
+	// create http server
+	http.ListenAndServe(":8080", nil)
+
+	// http using time.Ticker client to send pings
 }
 
 type Server struct {
